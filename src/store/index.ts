da82cdae690e2cafaa1fbe74fd89f3af2207f067
ToMY <tomy.develop@gmail.com>
2018-11-17 23:05:45 +0900

    Divide store into modules

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import sample from './modules/sample'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  modules: {
+    sample
+  },
+  strict: debug
+})
